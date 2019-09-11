d8c2f63da8e4ded781ea03817897457d72db5268
gobinath <gobinath164026@gmail.com>
2019-09-11 22:05:22 +0530

    update

---
+++ weight_on_planetx/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import './ui/home.dart';
+void main() {
+  runApp(new MaterialApp(
+    title: "Weight Of The Planetx",
+    home: new Home(),
+  ));
+}
---
+++ weight_on_planetx/lib/ui/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new HomeState ();
+  }
+
+}
+
+class HomeState extends State<Home> {
+  int radioValue = 0;
+  void handleRadioValueChanged(int value){
+    setState(() {
+      radioValue = value;
+          print(radioValue);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Weight On Planet x"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.red,
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new ListView(
+          padding: const EdgeInsets.all(2.5),
+          children: <Widget>[
+            new Image.asset('images/planet.png',
+            height: 133.0,
+            width: 200.0,),
+
+
+            new Container(
+              margin: const EdgeInsets.all(3.0),
+              alignment: Alignment.center,
+              child: new Column(
+                children: <Widget>[
+                  new TextField(
+                    controller: null,
+                    keyboardType: TextInputType.number,
+                    decoration: new InputDecoration(
+                      labelText: 'Your Weight On Earth',
+                      hintText: 'In Pounds',
+                      icon: new Icon(Icons.person_outline)
+                    ),
+                  ),
+                    
+
+                    ],
+
+                      )
+                    ],
+                  )
+                    
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
