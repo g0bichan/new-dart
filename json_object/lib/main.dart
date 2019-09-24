47ef882495395ee7d6a9485b05196d0ece1979b2
gobinath <gobinath164026@gmail.com>
2019-09-24 22:19:03 +0530

    update

---
+++ json_object/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  List _data = await getJson();
+  print(_data[1]['title']);
+  runApp(new MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.red),
+    home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text('JSON Parse'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: new Center(
+        child: new Text('Parsing'),
+      ),
+    ),
+  ));
+}
+
+Future<List> getJson() async {
+  String apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+  http.Response response = await http.get(apiUrl);
+  return json.decode(response.body);
+}
