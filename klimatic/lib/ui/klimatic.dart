7fa91b9fa5f8fdf1af2887268c2374c8a0d3c949
gobinath <gobinath164026@gmail.com>
2019-10-06 23:36:20 +0530

    upda

--- klimatic/lib/ui/klimatic.dart
+++ klimatic/lib/ui/klimatic.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Klimatic extends StatefulWidget {
@@ -11,9 +12,39 @@ class _KlimaticState extends State<Klimatic> {
     return Scaffold(
       appBar: new AppBar(
         title: new Text('Klimatic'),
-        backgroundColor: Colors.white,
-        actions: <Widget>[],
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.menu),
+              onPressed: ()=> debugPrint("Hey"))
+        ],
+      ),
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new Image.asset('images/umbrella.png',
+            width: 490.0,
+            height: 1200.0,
+            fit: BoxFit.fill,
+            ),
+          ),
+          new Container(
+            alignment: Alignment.topRight,
+            margin: const EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
+            child: new Text('Spokane',
+            style: new TextStyle(),
+            ),
+
+          )
+        ],
       ),
     );
   }
+}
+TextStyle cityStyle() {
+  return new TextStyle(
+    color: Colors.white,
+    fontSize: 22.9,
+    fontStyle: FontStyle.italic
+  );
 }
