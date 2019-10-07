aae2bbb5a42680905ee93b1c2ce70d3322131584
gobinath <gobinath164026@gmail.com>
2019-10-07 17:54:09 +0530

    jtjt

--- klimatic/lib/ui/klimatic.dart
+++ klimatic/lib/ui/klimatic.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import '../util/util.dart' as util;
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart'as http;
 class Klimatic extends StatefulWidget {
   @override
   _KlimaticState createState() => _KlimaticState();
 }
 
 class _KlimaticState extends State<Klimatic> {
+
+  void showStuff() async {
+    Map data  = await getWeather(util.appId, util.defaultCity);
+    print(data.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +25,7 @@ class _KlimaticState extends State<Klimatic> {
         backgroundColor: Colors.redAccent,
         actions: <Widget>[
           new IconButton(icon: new Icon(Icons.menu),
-              onPressed: ()=> debugPrint("Hey"))
+              onPressed: showStuff)
         ],
       ),
       body: new Stack(
@@ -34,11 +43,30 @@ class _KlimaticState extends State<Klimatic> {
             child: new Text('Spokane',
             style: new TextStyle(),
             ),
+          ),
+          new Container(
+            alignment: Alignment.center,
+            child: new Image.asset('images/rain.png'),
+
+          ),
+          new Container(
+            margin: const EdgeInsets.fromLTRB(30.0, 290.0, 0.0, 0.0),
+            child: new Text('77.5F',
+            style: tempstyle(),
+            ),
 
           )
         ],
       ),
     );
+  }
+
+  Future<Map> getWeather(String appId, String city) async {
+    String apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=$city&appid='
+    '${util.appId}';
+    http.Response response = await http.get(apiUrl);
+    return json.decode(response.body);
+
   }
 }
 TextStyle cityStyle() {
@@ -46,5 +74,15 @@ TextStyle cityStyle() {
     color: Colors.white,
     fontSize: 22.9,
     fontStyle: FontStyle.italic
+  );
+}
+
+TextStyle tempstyle(){
+  return new TextStyle(
+    color: Colors.white,
+    fontSize: 49.9,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w900
+
   );
 }
