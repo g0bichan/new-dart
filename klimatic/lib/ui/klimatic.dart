59d2931ff9c72bc97ed1b93f23b2e48145a6f3c7
gobinath <gobinath164026@gmail.com>
2019-10-13 13:32:40 +0530

    jgf

--- klimatic/lib/ui/klimatic.dart
+++ klimatic/lib/ui/klimatic.dart
@@ -11,6 +11,21 @@ class Klimatic extends StatefulWidget {
 }
 
 class _KlimaticState extends State<Klimatic> {
+String _cityEntered;
+Future _goToNextScreen(BuildContext context)async{
+  Map result = await Navigator.of(context).push(
+    new MaterialPageRoute<Map>(builder: (BuildContext context){
+      return new ChangeCity();
+    })
+  );
+if (result != null && result.containsKey('enter')){
+  _cityEntered = result['enter'];
+}
+
+
+}
+
+
   void showStuff() async {
     Map data = await getWeather(util.appId, util.defaultCity);
     print(data.toString());
@@ -24,7 +39,8 @@ class _KlimaticState extends State<Klimatic> {
         centerTitle: true,
         backgroundColor: Colors.redAccent,
         actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.menu), onPressed: showStuff)
+          new IconButton(icon: new Icon(Icons.menu),
+              onPressed: (){_goToNextScreen(context);})
         ],
       ),
       body: new Stack(
@@ -41,7 +57,7 @@ class _KlimaticState extends State<Klimatic> {
             alignment: Alignment.topRight,
             margin: const EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
             child: new Text(
-              'Spokane',
+              '${_cityEntered == null ? util.defaultCity : _cityEntered}',
               style: cityStyle(),
             ),
           ),
@@ -49,10 +65,10 @@ class _KlimaticState extends State<Klimatic> {
             alignment: Alignment.center,
             child: new Image.asset('images/rain.png'),
           ),
-          new Container(
-            margin: const EdgeInsets.fromLTRB(30.0, 290.0, 0.0, 0.0),
-            child: updateTempWidget('chennai'),
-          ),
+//          new Container(
+//            margin: const EdgeInsets.fromLTRB(30.0, 290.0, 0.0, 0.0),
+            updateTempWidget(_cityEntered)
+          //),
         ],
       ),
     );
@@ -68,12 +84,14 @@ class _KlimaticState extends State<Klimatic> {
 
   Widget updateTempWidget(String city) {
     return new FutureBuilder(
-        future: getWeather(util.appId, city),
+        future: getWeather(util.appId, city == null ? util.defaultCity : city),
         builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
           if (snapshot.hasData) {
             Map content = snapshot.data;
             return new Container(
+              margin: const EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
               child: new Column(
+//                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   new ListTile(
                     title: new Text(content['main']['temp'].toString(),
@@ -82,6 +100,17 @@ class _KlimaticState extends State<Klimatic> {
                             fontSize: 65.9,
                             color: Colors.green,
                             fontWeight: FontWeight.w500)),
+
+                    subtitle: new ListTile(
+                      title:  new Text(
+                        "Humidity: ${content['main']['humidity'].toString()}\n"
+                            "Min: ${content['main']['temp_min'].toString()} F\n"
+                            "Max: ${content['main']['temp_max'].toString()} F",
+
+                        style: extraData(),
+
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -92,39 +121,70 @@ class _KlimaticState extends State<Klimatic> {
         });
   }
 }
-//
-//class ChangeCity extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return new Scaffold(
-//      appBar: new AppBar(
-//        backgroundColor: Colors.red,
-//        title: new Text(''),
-//        centerTitle: true,
-//      ),
-//      body: new Stack(
-//        children: <Widget>[
-//          new ListView(
-//            children: <Widget>[
-//              new Image.asset(
-//                'images/snow.png',
-//                width: 490.0,
-//                height: 1200.0,
-//                fit: BoxFit.fill,
-//              )
-//            ],
-//          )
-//        ],
-//      ),
-//    );
-//  }
-//}
+
+class ChangeCity extends StatelessWidget {
+  var _cityFieldController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: new Text('Next'),
+        centerTitle: true,
+      ),
+      body: new Stack(
+        children: <Widget>[
+        new Center(
+          child: new Image.asset('images/snow.png',
+          width: 490.0,
+            height: 1200.0,
+            fit: BoxFit.fill,
+          ),
+        ),
+          new ListView(
+            children: <Widget>[
+              new ListTile(
+                title: new TextField(
+                  decoration: new InputDecoration(
+                   hintText: 'Enter City',
+                  ),
+                  controller: _cityFieldController,
+                  keyboardType: TextInputType.text,
+                ),
+              ),
+
+              new ListTile(
+                title: new FlatButton(onPressed: () {
+                  Navigator.pop(context,{
+                    'enter': _cityFieldController.text
+                  });
+                },
+                    textColor: Colors.white70,
+                    color: Colors.redAccent,
+                    child: new Text('Get Weather')),
+              )
+
+            ],
+          )
+
+        ],
+      ),
+    );
+  }
+}
 
 TextStyle cityStyle() {
   return new TextStyle(
       color: Colors.white, fontSize: 22.9, fontStyle: FontStyle.italic);
 }
 
+TextStyle extraData() {
+  return new TextStyle(
+    color: Colors.white70,
+    fontStyle: FontStyle.normal,
+    fontSize: 17.0
+  );
+}
 TextStyle tempstyle() {
   return new TextStyle(
       color: Colors.white,
--- klimatic/lib/util/util.dart
+++ klimatic/lib/util/util.dart
@@ -1,2 +1,2 @@
 final appId = "ef813a1a983233a7fd7acb149ba5080d";
-final defaultCity = "chennai";
+final defaultCity = "Spokane";
