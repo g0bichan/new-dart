587721783fb863bf087f5fd1e1c9da3db66d6ab3
gobinath <54366974+g0bichan@users.noreply.github.com>
2019-11-26 16:33:08 +0530

    fggg

--- firebase_set/lib/main.dart
+++ firebase_set/lib/main.dart
@@ -1,10 +1,15 @@
-import 'dart:async';
+//import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_database/firebase_database.dart';
+//import 'package:firebase_database/ui/firebase_animated_list.dart';
+//import 'model/board.dart';
 
-import 'model/board.dart';
 
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = new GoogleSignIn();
 
 
 void main() => runApp(MyApp());
@@ -14,9 +19,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Community Board',
+      title: 'auth-firebase',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -25,26 +29,27 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- // int _counter = 0;
-  List<Board> boardMessages = List();
-  Board board;
-  final FirebaseDatabase database = FirebaseDatabase.instance;
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  DatabaseReference databaseReference;
-
-  @override
-  void initState() {
-    super.initState();
-    board = Board("","");
-    databaseReference = database.reference().child("community_board");
-    databaseReference.onChildAdded.listen(_onEntryAdded);
-  } //  void _incrementCounter() {
+  String _imageUrl;
+  // int _counter = 0;
+//  List<Board> boardMessages = List();
+//  Board board;
+//  final FirebaseDatabase database = FirebaseDatabase.instance;
+//  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+//  DatabaseReference databaseReference;
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//    board = Board("", "");
+//    databaseReference = database.reference().child("community_board");
+//    databaseReference.onChildAdded.listen(_onEntryAdded);
+//    databaseReference.onChildChanged.listen(_onEntryChanged);
+  //  void _incrementCounter() {
 //
 ////    database.reference().child("Message").set({
 ////      "firstname" : "gobi",
@@ -63,69 +68,54 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text("Board"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            flex:0,
-            child: Form(
-              key: formKey,
-              child: Flex(direction: Axis.vertical,
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.subject),
-                  title: TextFormField(
-                    initialValue: "",
-                    onSaved: (val) => board.subject = val,
-                    validator: (val) => val == "" ? val : null,
-
-                  ),
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: new Text("Board"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  child: Text("google-sign"),
+                  onPressed: () => _googleSignin(),
+                  color: Colors.red,
                 ),
-                ListTile(
-              leading: Icon(Icons.message),
-          title: TextFormField(
-            initialValue: "",
-            onSaved: (val) => board.body = val,
-            validator: (val) => val == "" ? val : null,
-          ),
-                ),
-
-                FlatButton(
-                  child: Text("post"),
-                  color: Colors.redAccent ,
-                  onPressed: (){
-                    handlesubmit();
-                  },
-                )
-
-
-              ],
               ),
-            ),
-          )
-        ],
-      )
-    );
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                    color: Colors.purple,
+                    child: Text("signin with email"),
+                    onPressed: () {}),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                    color: Colors.orange,
+                    child: Text("create account"),
+                    onPressed: () {},
+                ),
+              ),
+              new Image.network(_imageUrl == null || _imageUrl.isEmpty ?
+              )
+            ],
+          ),
+        ));
   }
 
-  void _onEntryAdded(Event event) {
-    setState(() {
-      boardMessages.add(Board.formSnapshot(event.snapshot));
-    });
-  }
+ Future<FirebaseUser> _googleSignin() async {
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
 
-  void handleSubmit() {
-   final FormState form = formKey.currentState;
-   if(form.validate()){
-     form.save();
-     form.reset();
-     databaseReference.push().set(board.toJson());
-   }
+    FirebaseUser user = await _auth.signInWithGoogle(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+    print("User is: ${user.photoUrl}");
+    return user;
   }
 }
